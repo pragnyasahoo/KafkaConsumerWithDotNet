683bcce3af81e07bfbb5eb97971193e4510d735f
Pragnya Sahoo <pragnya.pradip@gmail.com>
2022-06-12 00:23:49 +0530

    Add project files.

---
+++ KafkaConsumer/Handler/KafkaConsumerHandler.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace KafkaConsumer.Handler
+{
+    public class KafkaConsumerHandler : IHostedService
+    {
+        private readonly string topicName = "mytesttopic";
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "consumer_group_id",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using (var builder = new ConsumerBuilder<Ignore,
+                string>(conf).Build())
+            {
+                builder.Subscribe(topicName);
+                var cancelToken = new CancellationTokenSource();
+                try
+                {
+                    while (true)
+                    {
+                        var consumer = builder.Consume(cancelToken.Token);
+                        Console.WriteLine($"Message: {consumer.Message.Value} received from TopicPartition of {consumer.TopicPartitionOffset}");
+                    }
+                }
+                catch (Exception)
+                {
+                    builder.Close();
+                }
+            }
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ KafkaProducer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaProducer.Controllers
+{
+    [Route("api/kafka")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig {   BootstrapServers = "localhost:9092" };
+
+
+        private readonly string topicName = "mytesttopic";
+       
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created(string.Empty, SendMessageToKafka(topicName, message));
+        }
+        private Object SendMessageToKafka(string topicName, string message)
+        {
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return producer.ProduceAsync(topicName, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($" something went wrong in Producer: {e}");
+                }
+            }
+            return null;
+        }
+    }
+}
